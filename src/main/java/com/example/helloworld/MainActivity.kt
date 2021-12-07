c7ae76e1eef965076771d4db114d87a8d27fb182
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2021-12-07 22:22:51 +0200

    Painter implemented

--- src/main/java/com/example/helloworld/MainActivity.kt
+++ src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,112 +1,70 @@
 package com.example.helloworld
 
-import android.content.Intent
-import android.net.Uri
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Xml
-import android.view.View
-
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
+import android.widget.ImageButton
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.*
+import com.example.helloworld.PaintView.Companion.colorList
+import com.example.helloworld.PaintView.Companion.currentBrush
+import com.example.helloworld.PaintView.Companion.pathList
 
-class MainActivity : AppCompatActivity(), UpdateAndDelete {
 
-    lateinit var database:DatabaseReference
-    var toDoList: MutableList<ToDoModel>? = null
-    lateinit var adapter: ToDoAdapter
+class MainActivity : AppCompatActivity(){
 
-    private var listViewItem: ListView? = null
+    companion object {
+        var path = Path()
+        var paintBrush = Paint()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        setListenersForBtns()
+    }
 
-        listViewItem = findViewById(R.id.item_listView) as ListView
+    private fun setListenersForBtns() {
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val greenBtn = findViewById<ImageButton>(R.id.greenColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val eraserBtn = findViewById<ImageButton>(R.id.eraser)
 
-
-        database = FirebaseDatabase.getInstance().reference
-
-        fab.setOnClickListener {view ->
-            val alertDialog = AlertDialog.Builder(this)
-            val textEditText = EditText(this)
-            alertDialog.setMessage("Add TODO Item")
-            alertDialog.setTitle("Enter ToDo item")
-            alertDialog.setView(textEditText)
-            alertDialog.setPositiveButton("Add") {dialog, i ->
-                val  toDoItemData = ToDoModel.createList()
-                toDoItemData.itemDataText = textEditText.text.toString()
-                toDoItemData.done = false
-
-                val newItemData = database.child("todo").push()
-                toDoItemData.Uid = newItemData.key
-
-                newItemData.setValue(toDoItemData)
-
-                dialog.dismiss()
-                Toast.makeText(this, "item saved", Toast.LENGTH_LONG).show()
-            }
-
-            alertDialog.show()
+        redBtn.setOnClickListener {
+            Toast.makeText(this, "Red licked", Toast.LENGTH_LONG).show()
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
         }
 
-        toDoList = mutableListOf<ToDoModel>()
-        adapter = ToDoAdapter(this, toDoList!!)
-        listViewItem!!.adapter = adapter
-
-        database.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(applicationContext, "No item added", Toast.LENGTH_LONG).show()
-            }
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                toDoList!!.clear()
-                addItemToList(snapshot)
-            }
-        })
-
-    }
-
-    private fun addItemToList(snapshot: DataSnapshot) {
-        val items = snapshot.children.iterator()
-
-        if (items.hasNext()) {
-            val toDoIndexedValue = items.next()
-            val itemsIterator = toDoIndexedValue.children.iterator()
-
-            while (itemsIterator.hasNext()) {
-                val currentItem = itemsIterator.next()
-                val toDoItemData = ToDoModel.createList()
-
-                val map = currentItem.getValue() as HashMap<String, Any>
-
-                toDoItemData.Uid = currentItem.key
-                toDoItemData.done = map.get("done") as Boolean?
-                toDoItemData.itemDataText = map.get("itemDataText") as String?
-                toDoList!!.add(toDoItemData)
-            }
+        greenBtn.setOnClickListener {
+            Toast.makeText(this, " Green Clicked", Toast.LENGTH_LONG).show()
+            paintBrush.color = Color.GREEN
+            currentColor(paintBrush.color)
         }
 
-        adapter.notifyDataSetChanged()
+        blueBtn.setOnClickListener {
+            Toast.makeText(this, "Blue Clicked", Toast.LENGTH_LONG).show()
+            paintBrush.color = Color.BLUE
+            currentColor(paintBrush.color)
+        }
+
+        eraserBtn.setOnClickListener {
+            Toast.makeText(this, "Eraser Clicked", Toast.LENGTH_LONG).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+
+        }
+
+
     }
 
-    override fun modifyItem(itemUid: String, isDone: Boolean) {
-        val itemReference = database.child("todo").child(itemUid)
-        itemReference.child("done").setValue(isDone)
+    private fun currentColor(color: Int) {
+        currentBrush = color
+        path = Path()
     }
-
-    override fun onItemDelete(itemUid: String) {
-        val itemReference = database.child("todo").child(itemUid)
-        itemReference.removeValue()
-        adapter.notifyDataSetChanged()
-    }
-
 
 }
